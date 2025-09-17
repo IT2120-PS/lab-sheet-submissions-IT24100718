172c90dce699ffd02d58d68d32335937a033dbc2
IT24100718 <it24100718@my.sliit.lk>
2025-09-17 10:38:17 +0530

    Adding Lab 07

---
+++ Lab Sheet 07/IT24100718 Lab 07.R
@@ -0,0 +1,12 @@
+setwd("//Users//sithijavithanage//Library//CloudStorage//OneDrive-SriLankaInstituteofInformationTechnology//Y2 S1//Probability and Statistics - IT2120//LAB 7")
+#question 1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#question 2
+pexp(2, rate = 1/3)
+
+#question 3
+#part 1
+1 - pnorm(130, mean = 100, sd = 15)
+#part 2
+qnorm(0.95, mean = 100, sd = 15)
