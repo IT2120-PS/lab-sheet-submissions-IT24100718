c1a4cb1f214326d02c4465d1da078644736b3edc
IT24100718 <it24100718@my.sliit.lk>
2025-10-01 22:06:09 +0530

    Adding lab 09

---
+++ Lab Sheet 09/IT24100718.R
@@ -0,0 +1,9 @@
+setwd("//Users//sithijavithanage//Library//CloudStorage//OneDrive-SriLankaInstituteofInformationTechnology//Y2 S1//Probability and Statistics - IT2120//LAB 9//Lab 9")
+##Exercise
+#1).
+bake <- rnorm(25, mean = 45, sd = 2)
+bake
+
+#2).
+t.test(bake, mu = 46, alternative = "less")
+
